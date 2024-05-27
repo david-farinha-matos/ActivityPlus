e441fd95463ec2d5430a62c67a163f75d250d760
David Matos <davidmatos@btinernet.com>
2024-05-27 14:02:33 +0100

    Initial Commit

---
+++ ActivityPlus/ActivityPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityPlusApp.swift
+//  ActivityPlus
+//
+//  Created by David Matos on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ActivityPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
